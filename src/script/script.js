ac7d43b014878bb933d9a17d1ecae68be576f04c
User <dit7766@gmail.com>
2025-08-23 23:41:26 -0300

    criei uma nova funcionalidade: carrinho.js.Como toda as odificação .

--- src/script/script.js
+++ src/script/script.js
@@ -1,5 +1,7 @@
 import temporizador from "./modules/temporizador.js";
 import hero from './modules/produtos.js';
+import carrinho from './modules/carrinho.js';
 
+carrinho();
 hero();
 temporizador();
